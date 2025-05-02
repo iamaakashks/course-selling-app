3f8bfe7250a12de836353d751931b0391406ba03
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-05-03 03:19:47 +0530

    add routes endpoint

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import './config/dotenv.js'
+import authRoutes from './routes/auth_routes.js';
+import courseRoutes from './routes/course_routes.js';
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send({msg: "Welcome to Home page"})
 })
+
+app.use('/auth/user', authRoutes);
+app.use('/course', courseRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is live at PORT ${PORT}`)
---
+++ routes/course_routes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const courseRouter = express.Router();
+
+courseRouter.get('/my_courses', (req, res)=>{
+
+})
+
+courseRouter.get('/all_courses', (req, res)=>{
+    
+})
+
+export default courseRouter;
